0d14e093f918896c22c5761ab7c1b072afecaea6
Abdelaziz Elgeziry <abdelazizadel99@gmail.com>
2020-12-14 11:50:26 +0200

    initial commit

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const { app } = require('./app.js');
+const dotenv = require('dotenv');
+dotenv.config();
+mongoose.connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Main database connected successfully');
+});
+app.listen(process.env.PORT, () => {
+    console.log(`Server listening at port ${process.env.PORT}`);
+});
