46733dad8bfaa4f6faf4d2ad4e539629059412ea
Mazen <mazen.ashraf.12@gmail.com>
2020-12-14 23:27:02 +0200

    modified the memberId in presence model

--- server/models/Presence.js
+++ server/models/Presence.js
@@ -3,6 +3,6 @@ const Schema = mongoose.Schema;
 const PresenceSchema = new Schema({
     startDate:Date,
     endDate: Date,
-    memberID: {type:Schema.Types.ObjectId, ref:'Member'}
+    memberID: String
 });
 module.exports = {model:mongoose.model('Presence', PresenceSchema),schema:PresenceSchema};
