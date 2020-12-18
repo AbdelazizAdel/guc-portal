d967a1e171eb0c3abffe70298ae27c8e14bea225
Abdelaziz Elgeziry <abdelazizadel99@gmail.com>
2020-12-18 21:51:11 +0200

    added view profile route tests

--- tests/general/login.test.js
+++ tests/general/login.test.js
@@ -60,3 +60,29 @@ describe("testing logout route", () => {
         expect(dbRes.loggedIn).not.toBeTruthy();
     })
 })
+
+describe("testing view profile route", () => {
+
+    test("testing view profile route when logged in", async() => {
+        const {member, plainTextPassword} = await createMember();
+        await member.save();
+        const response = await request.post('/login').send({email : member.email, password : plainTextPassword});
+        const token = response.headers.auth_token;
+        const profile = await request.get('/profile').set('auth_token', token);
+        expect(profile.body.id).toMatch(member.id);
+        expect(profile.body.email).toMatch(member.email);
+        expect(profile.body.password).toMatch(member.password);
+        expect(profile.body.loggedIn).toBeTruthy();
+    })
+
+    test("testing view profile route when logged out", async() => {
+        const {member, plainTextPassword} = await createMember();
+        await member.save();
+        const response = await request.post('/login').send({email : member.email, password : plainTextPassword});
+        const token = response.headers.auth_token;
+        await request.get('/logout').set('auth_token', token);
+        const profile = await request.get('/profile').set('auth_token', token);
+        expect(profile.text).toMatch("Please Log in to view your profile");
+    })
+
+})
