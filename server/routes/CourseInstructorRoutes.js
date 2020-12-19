74d69ea31044265c4c9611d096e88cf83c206e24
Mazen <mazen.ashraf.12@gmail.com>
2020-12-19 22:47:39 +0200

    added course Coverage route

---
+++ server/routes/CourseInstructorRoutes.js
@@ -0,0 +1,115 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const StaffMember = require('../models/StaffMember.js');
+const Slot = require('../models/Slot.js');
+const Course = require('../models/Course.js');
+const router = express.Router();
+const dummmy = require('mongoose-dummy');
+const ignoredFields = ['_id', '__v'];
+// const Authenticate = (req,res,next)=>{
+//     const token = req.headers.authorization.split(' ')[1];
+//     console.log(token);
+//     console.log(process.env.TOKEN_SECRET);
+//     jwt.verify(token,process.env.TOKEN_SECRET,(err,id)=>{
+//         if(err){
+//             console.log(err);
+//              return res.sendStatus('404');
+//              return 0;
+//         }
+
+//         req.body.id= id;
+//         next();
+//     });
+// }
+router.use(express.json());
+router.post('/StaffMembers',(req,res)=>{
+
+    try{
+        const member = new StaffMember({
+            "id": req.body.id,
+            "name": req.body.name,
+            "email": req.body.email,
+            "gender": req.body.gender,
+            "salary": req.body.salary,
+            "dayOff": req.body.dayOff,
+            "leaves": req.body.leaves,
+            "password": req.body.password,
+            "startDay": req.body.startDay,
+            "loggedIn": req.body.loggedIn,
+            "officeLoc": req.body.officeLoc,
+            "attendance": req.body.attendance
+          })
+        console.log(typeof(member));
+        member.save((err,doc)=>{
+            if(err) return console.log(err);
+            console.log('Document inserted successfully');
+        });
+        res.status(200).send('Member added successfully');
+    }
+    catch(err) {
+        console.log(err);
+        res.status(404).send('fih moshkla ya mealem');
+
+    }
+})
+router.post('/Courses',(req,res)=>{
+    try {
+        course = new Course({
+        'id':req.body.id,
+        'name':req.body.name,
+        'coordinator':req.body.coordinator,
+        'TAs':req.body.TAs,
+        'instructors':req.body.instructors,
+        'numSlots':req.body.numSlots
+    });
+    course.save((err,doc)=>{
+        if(err) return console.log(err);
+        console.log('Course inserted successfully');
+    });
+    res.status(200).send('course added successfully');
+}
+catch(err){
+    console.log(err);
+    res.status(404).send('fih moshkla ya mealem');
+}
+});
+router.post('/Slots',(req,res)=>{
+    try {
+        slot = new Slot({
+        'id':req.body.id,
+        'day':req.body.day,
+        'period':req.body.period,
+        'location':req.body.location,
+        'slotType':req.body.slotType,
+        'course':req.body.course,
+        'instructor':req.body.instructor
+    });
+    slot.save((err,doc)=>{
+        if(err) return console.log(err);
+        console.log('slot created successfully');
+    });
+    res.status(200).send('slot created successfully');
+}
+catch(err){
+    console.log(err);
+    res.status(404).send('fih moshkla ya mealem');
+}
+});
+router.get('/members/:id/coverage',async(req,res)=>{
+    const memberId = req.params.id;
+    const memberCourses =await Course.find().or([{'instructors':{"$in":`${memberId}`}},{'TAs':{"$in":`${memberId}`}}]);
+    const response = {}; 
+    for(let i=0;i<memberCourses.length;i++){
+        const courseSlots = await Slot.find().and([{'instructor':{"$exists":true}},{'course':memberCourses[i].id}]);
+        const count = courseSlots.length;
+        response[`${memberCourses[i].name}`] = 100.0*count/memberCourses[i].numSlots;
+    }
+    res.status(200).send(response);
+
+});
+
+
+
+module.exports = router;
