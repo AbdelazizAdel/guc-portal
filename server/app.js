62a124ce58cee70983458fc12273b4aeac0e0475
Mazen <mazen.ashraf.12@gmail.com>
2020-12-19 23:01:54 +0200

    modified the app.js to add course instructor routes

--- server/app.js
+++ server/app.js
@@ -6,7 +6,8 @@ app.use(express.json());
 
 // middleware for using the general routes
 const generalRoutes = require('./routes/general.js');
+//middleware for using the Course Instructor routes
+const CourseInstructorRoutes = require('./routes/CourseInstructorRoutes.js');
 app.use(generalRoutes);
-
-
+app.use(CourseInstructorRoutes);
 module.exports.app = app;
