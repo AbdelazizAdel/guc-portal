e766d421af3cb44500d95660fba9ab3213d906bf
Mazen <mazen.ashraf.12@gmail.com>
2020-12-19 23:06:32 +0200

    resolving app.js conflict

--- server/app.js
+++ server/app.js
@@ -6,8 +6,7 @@ app.use(express.json());
 
 // middleware for using the general routes
 const generalRoutes = require('./routes/general.js');
-//middleware for using the Course Instructor routes
-const CourseInstructorRoutes = require('./routes/CourseInstructorRoutes.js');
 app.use(generalRoutes);
-app.use(CourseInstructorRoutes);
+
+
 module.exports.app = app;
