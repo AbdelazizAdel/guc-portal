14459f7759612f36728bb205d780deee7848d33d
Mazen <mazen.ashraf.12@gmail.com>
2020-12-20 20:03:08 +0200

    Added Assigning slots route and it's tests

--- server/routes/CourseInstructorRoutes.js
+++ server/routes/CourseInstructorRoutes.js
@@ -90,6 +90,67 @@ router.get('/members/:id/coverage',async(req,res)=>{
     res.status(200).send(response);
 
 });
+// this route returns the courseIds this instructor is responsible for
+router.get('/instructors/:id/courses',async(req,res)=>{
+    const instructorId = req.params.id;
+    const instructorCourses = await Course.find({'instructors':{"$in":`${instructorId}`}});
+    //console.log(instructorCourses);
+    if(instructorCourses.length == 0){
+        return res.status(403).send('You are not allowed to assign slots');
+    }
+    let coursesId = [];
+    for(let i =0 ; i<instructorCourses.length;i++){
+        coursesId.push(instructorCourses[i].id);
+    }
+    const response ={courses:coursesId};
+    res.status(200).send(response);
+});
+router.get('/instructors/:id/courses/:courseid',async(req,res)=>{
+    const instructorId = req.params.id;
+    const courseId = req.params.courseid;
+    const course = await Course.findOne({'id':`${courseId}`});
+    console.log(course.instructors);
+    if(!course.instructors.includes(instructorId)){
+        return res.status(403).send('You are not allowed to view these course Information');
+    }
+    courseTAs = [...course.TAs];
+    courseInstructors = [...course.instructors];
+    const unAssignedSlots = await Slot.find().and([{'course':courseId},{'instructor':{"$exists":false}}]);
+    response= {unAssignedSlots:unAssignedSlots,courseTAs:courseTAs,courseInstructors:courseInstructors};
+    res.status(200).send(response);
+});
+/**
+ *  This request requires a body in the following structure
+ * {
+ *  instructorId :value,
+ *  courseId :value
+ * }
+ */
+router.patch('/academic-members/:memberId/slots/:slotId',async(req,res)=>{
+    const academicMemberId = req.params.memberId;
+    const slotId =req.params.slotId;
+    const instructorId = req.body.instructorId;
+    const courseId = req.body.courseId;
+    const member = await StaffMember.find({'id':academicMemberId});
+    if(member.length === 0){ 
+        return res.status(404).send('The member you are trying to assign is not found!!');}
+    
+    const course = await Course.findOne({'id':courseId});
+    if(!course.instructors.includes(`${instructorId}`)){
+        return res.status(403).send('You are not allowed to assign a slot to an academic member!!');
+    }
+    if(!(course.instructors.includes(`${academicMemberId}`)||course.TAs.includes(`${academicMemberId}`))){
+        return res.status(403).send('This teaching assistant can\'t be assigned to this course');
+    }
+    const slot = await Slot.findOne({'id':slotId});
+    const isMemberfree = await Slot.find().and([{'instructor':academicMemberId},{'day':slot.day},{'period':slot.period}]);
+    if(isMemberfree.length!==0){
+        return res.status(403).send('This staff member is not available to be assigned to this slot!!');
+    }
+    await Slot.updateOne({"id":slotId},{"instructor":academicMemberId});
+    res.status(200).send('The Slot was modified correctly');
+
+})
 
 
 
