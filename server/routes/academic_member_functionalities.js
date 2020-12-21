95a3288f9a12c482818a0292457b773c48d1cbb6
ZIAX <ziad.tamer99@yahoo.com>
2020-12-21 16:21:01 +0200

    fixed routes and added enums to Request module

--- server/routes/academic_member_functionalities.js
+++ server/routes/academic_member_functionalities.js
@@ -8,50 +8,58 @@ const Attendance = require('../models/Attendance.js');
 const Course = require('../models/Course.js');
 const Faculty = require('../models/Faculty.js');
 const Replacement = require('../models/ReplacementSlot.js');
-const authentication = require('./middleware.js');
+const Authentication = require('./middleware.js');
+const MetaData = require('../models/metaData.js');
 
 router.use(express.json());
 
-router.get('/schedule', [authentication],(req, res)=>{
+router.get('/schedule', [Authentication],(req, res)=>{
 
-    let staffId = req.body.id;
+    try{
 
-    let schedule = [];
+        let staffId = req.body.member.id;
 
-    let scheule = Slot.filter((slot) => {
-        return slot.instructor === staffId;
+        let schedule = [];
+
+        let scheule = Slot.find.and([{'instructor':{"$in":`${staffId}`}}]);
+
+        let replacements = Replacement.filter.and([{'instructor':{"$in":`${staffId}`}}])
+        scheule = schedule.concat(replacements);
+        response = {'schedule' : schedule};
+        res.status(200).send(response);
     }
-    );
-
-    let replacements = Replacement.filter((replacement) => {
-        return replacement.instructor === staffId;
-    });
-    scheule = schedule.concat(replacements);
-    return schedule;
+    catch(err){
+            console.log(err);
+            res.status(404).send('fe blabizo 7asal hena');
+    }
 });
 
-router.post('/replacement/add', [authentication], (req, res) => {
+router.post('/replacement/request', [Authentication], (req, res) => {
     try{
-        const replacementSlot = new Replacement({
-        id: {type: String, required: true, unique: true},
-        day: Number,
-    period: Number,
-    date: Date, 
-    location: String, 
-    slotType: String, 
-    course: String,
-    instructor: String
-          })
-        console.log(typeof(member));
-        member.save((err,doc)=>{
-            if(err) return console.log(err);
-            console.log('Document inserted successfully');
+        requestId = MetaData.find().and([{'sequenceName':{"$in":`replacementSlot`}}])['lastId'];
+        const request = new Request({
+            id: requestId,
+            sender: req.body.member.id,
+            receiver: req.body.receiver,
+            status: 'Pending',
+            content: req.body.content,
+            comment: req.body.comment,
+            type: 'replacementSlot',
+            submissionDate: Date.now(),
+            startDate: req.body.startDate,
+            duration: req.body.duration,
+            slot: req.body.slot,
+            attachmentURL : req.body.attachmentURL
         });
-        res.status(200).send('Member added successfully');
+        console.log(typeof(replacementSlot));
+        request.save((err, slot)=>{
+            if(err) return console.log(err);
+            console.log('Replacement request sent successfully');
+        });
+        res.status(200).send('Replacement request sent successfully');
     }
     catch(err) {
         console.log(err);
         res.status(404).send('fih moshkla ya mealem');
-
     }
 })
