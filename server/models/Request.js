50ff4e1ad2138537788125071e6f95b017afdfc0
morejas <54472095+morejas@users.noreply.github.com>
2020-12-25 23:49:10 +0200

    request model

--- server/models/Request.js
+++ server/models/Request.js
@@ -3,11 +3,7 @@ const mongoose = require('mongoose');
 const Slot = require("./Slot.js");
 
 const RequestScheme = new mongoose.Schema({
-<<<<<<< HEAD
     id: {type: String, unique: true, required: true},
-=======
-    id: String,
->>>>>>> 76a1e9e95f95a8a6f04f70d92363f6230ed971d4
     sender: String,
     receiver: String,
     status: {type:String, enum:['Accepted', 'Rejected', 'Pending']},
