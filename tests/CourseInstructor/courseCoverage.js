8da0b55d09659b448c2ead9ff9a33a43f1451a77
Mazen <mazen.ashraf.12@gmail.com>
2021-01-14 02:04:12 +0200

    added a part in tests to seed data

--- tests/CourseInstructor/courseCoverage.js
+++ tests/CourseInstructor/courseCoverage.js
@@ -44,8 +44,8 @@ describe('Testing Course Coverage route',()=>{
         await slot1.save();
         await slot2.save();
         const output =  await(await request.get('/instructors/coverage').set('auth_token',token)).body;
-        expect(output['Advanced Computer Lab']).toBe(0);
-        expect(output['SoftwareEngineering']).toBe(undefined);
+        expect(output['course1']).toBe(0);
+        expect(output['course2']).toBe(undefined);
     },10000)
     ,
     it('A case where the slot model will return based on Instructors',async ()=>{
@@ -61,8 +61,8 @@ describe('Testing Course Coverage route',()=>{
         await slot1.save();
         await slot2.save();
         const output = await (await request.get('/instructors/coverage').set('auth_token',token)).body;
-        expect(output['Advanced Computer Lab']).toBe(100.0/6);
-        expect(output['SoftwareEngineering']).toBe(undefined);
+        expect(output['course1']).toBe(100.0/6);
+        expect(output['course2']).toBe(undefined);
     },10000),
     it('A case where the slot model will return multiple records',async ()=>{
         const member =await createStaffMamber('Ashry19','ashry@yahoo.com','ashduia','Mohammed Ashry','male',7000,3,'C6.305',6,[],undefined,true,[],true,'dep1');
@@ -80,11 +80,13 @@ describe('Testing Course Coverage route',()=>{
         await slot2.save();
         await slot3.save();
         const output = await (await request.get('/instructors/coverage').set('auth_token',token)).body;
-        expect(output['Advanced Computer Lab']).toBe(2*100.0/10);
-        expect(output['SoftwareEngineering']).toBe(undefined);
+        expect(output['course1']).toBe(2*100.0/10);
+        expect(output['course2']).toBe(undefined);
     },10000),
     it('A case where the instructor is assigned to multiple courses',async ()=>{
         const member =await createStaffMamber('Ashry19','ashry@yahoo.com','ashduia','Mohammed Ashry','male',7000,3,'C6.305',6,[],undefined,true,[],true,'dep1');
+        const member2 =await createStaffMamber('Hany5','Hany@yahoo.com','ashduia','Hany Shawky','male',5600,3,'C2.305',6,[],undefined,true,[],true,'dep1');
+        const member3 =await createStaffMamber('sherif','sherif@yahoo.com','ashduia','sherif ayman','male',3000,3,'C5.305',6,[],undefined,true,[],true,'dep1');
         const response = await request.post('/login').send({email : member.email, password :'ashduia' });
         const token = response.headers.auth_token;
         const course1 = createCourse('course1','Advanced Computer Lab','Mervat22',['Hany5'],['Ashry19'],10);
@@ -99,7 +101,27 @@ describe('Testing Course Coverage route',()=>{
         await slot2.save();
         await slot3.save();
         const output = await (await request.get('/instructors/coverage').set('auth_token',token)).body;
-        expect(output['Advanced Computer Lab']).toBe(2*100.0/10);
-        expect(output['SoftwareEngineering']).toBe(100.0/10);
+        expect(output['course1']).toBe(2*100.0/10);
+        expect(output['course2']).toBe(100.0/10);
+    },10000),
+    it('A case where the instructor is assigned to multiple courses',async ()=>{
+        // const member =await createStaffMamber('Ashry19','ashry@yahoo.com','ashduia','Mohammed Ashry','male',7000,3,'C6.305',6,[],undefined,true,[],true,'dep1');
+        // const member2 =await createStaffMamber('Hany5','Hany@yahoo.com','ashduia','Hany Shawky','male',5600,3,'C2.305',6,[],undefined,true,[],true,'dep1');
+        const member3 =await createStaffMamber('sherif','sherif@yahoo.com','ashduia','sherif ayman','male',3000,3,'C5.305',6,[],undefined,true,[],true,'dep1');
+        // const response = await request.post('/login').send({email : member.email, password :'ashduia' });
+        // const token = response.headers.auth_token;
+        // const course1 = createCourse('course1','Advanced Computer Lab','Mervat22',['Hany5'],['Ashry19'],10);
+        // const course2 = createCourse('course2','SoftwareEngineering','Ayman6',['Menrit28'],['Ashry19'],10);
+        // const slot1 = createSlot('slot1',2,1,'C7.301','Lab','course2','Ashry19');
+        // const slot2 = createSlot('slot2',1,1,'C7.304','Lab','course1','Ashry19');
+        // const slot3 = createSlot('slot3',4,3,'C2.115','Lab','course1','Ashry19');
+
+        // await course1.save();
+        // await course2.save();
+        // await slot1.save();
+        // await slot2.save();
+        // await slot3.save();
+        // const output = await (await request.get('/instructors/coverage').set('auth_token',token)).body;
+        expect(1).toBe(1);
     },10000)
 })
